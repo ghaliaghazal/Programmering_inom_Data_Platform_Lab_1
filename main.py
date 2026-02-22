37259142c1658b60e3cc5d1d97c845bf5a4d94b5
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2026-02-22 06:10:58 +0100

    change with_price to missing_price in analytics summary

--- main.py
+++ main.py
@@ -1,60 +1,49 @@
-"""def main():
-    print("Hello from programmering-inom-data-platform-lab-1!")
-"""
+
 
 import pandas as pd 
 import numpy as np 
 
 if __name__ == "__main__":
     df = pd.read_csv("lab 1 - csv.csv", sep=';') 
-   # print(df.head()) 
+    
+    
+    df["name"] = df["name"].str.replace(r"\s+", " ", regex=True) 
+    df["name"] = df["name"].str.strip() 
+    df["name"] = df["name"].str.title()   
+    df["price"] = df["price"].str.replace(r"\s+", " ", regex=True)
 
-df["name"] = df["name"].str.replace(r"\s+", " ", regex=True) 
-df["name"] = df["name"].str.strip() 
-df["name"] = df["name"].str.title()   
-
-df["price"] = df["price"].str.replace(r"\s+", " ", regex=True)
-
-df["price"] = pd.to_numeric(df["price"], errors="coerce")
-df["price"] = df["price"].mask(df["price"] <= 0, np.nan)
-
+    df["price"] = pd.to_numeric(df["price"], errors="coerce")
+    df["price"] = df["price"].mask(df["price"] <= 0, np.nan)
 
 
-df["currency"] = df["currency"].str.replace(r"\s+", " ", regex=True)
-df["currency"] = df["currency"].str.strip()
-df["currency"] = df["currency"].str.upper()
+
+    df["currency"] = df["currency"].str.replace(r"\s+", " ", regex=True)
+    df["currency"] = df["currency"].str.strip()
+    df["currency"] = df["currency"].str.upper()
 
 
 
 
 
 # Flag missing values, and replace them with NaN
-df["id_missing"] = df["id"].isna()
-df["name_missing"] = df["name"].isna()
-df["price_missing"] = df["price"].isna()
-df["currency_missing"] = df["currency"].isna()
-df["created_at_missing"] = df["created_at"].isna()
+    df["id_missing"] = df["id"].isna()
+    df["name_missing"] = df["name"].isna()
+    df["price_missing"] = df["price"].isna()
+    df["currency_missing"] = df["currency"].isna()
+    df["created_at_missing"] = df["created_at"].isna()
 
 
-df["price_extreme"] = df["price"] > 15000 #flag extremely high prices
+    df["price_extreme"] = df["price"] > 15000 #flag extremely high prices
+    print(df)
 
-print(df)
 
-
-analytics_summary = pd.DataFrame({
+    analytics_summary = pd.DataFrame({
+        
+        "average_price": [df["price"].mean()],
+        "median_price": [df["price"].median()],
+        "number_of_products": [len(df)],
+        "number_of_products_missing_price": [df["price"].isna().sum()]
+        })
+    analytics_summary.to_csv("analytics_summary.csv", index=False)
+    print(analytics_summary)
     
-    "average_price": [df["price"].mean()],
-    "median_price": [df["price"].median()],
-    "number_of_products": [len(df)],
-    "number_of_products_with_price": [df["price"].notna().sum()]
-})
-analytics_summary.to_csv("analytics_summary.csv", index=False)
-
-print(analytics_summary)
-
-
-
-
-
-# print(df.info())
-# print(df.currency) 
